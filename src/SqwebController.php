746a943fb8aa7314d8239f46f657aefd871cd474
Mathieu Darrigade <mathieu@sqweb.com>
2017-06-19 15:33:22 +0200

    Changed a translation

--- src/SqwebController.php
+++ src/SqwebController.php
@@ -97,7 +97,7 @@ class SqwebController extends Controller
                     'already_sub'   => 'Déjà abonné ? ',
                     'login'         => 'Connexion',
                     'unlock'        => 'Débloquez ce contenu avec',
-                    'desc'          => 'Multipass est un abonnement multi-sites, sans engagement.',
+                    'desc'          => 'L\abonnement multi-sites, sans engagement.',
                     'href'          => 'https://www.multipass.net/fr/sites-partenaires-premium-sans-pub-ni-limites',
                     'discover'      => 'Découvrir tous les partenaires'
                 );
@@ -110,7 +110,7 @@ class SqwebController extends Controller
                     'already_sub'   => 'Already a member? ',
                     'login'         => 'Sign in',
                     'unlock'        => 'Unlock this content, get your ',
-                    'desc'          => 'Multipass is a multisite subscription, with no commitment.',
+                    'desc'          => 'The multisite subscription, with no commitment.',
                     'href'          => $href,
                     'discover'      => 'Discover all the partners'
                 );
