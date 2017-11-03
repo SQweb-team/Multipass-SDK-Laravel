260be36eb2df62cdbeb31228f802977c631906f6
Mathieu Darrigade <mathieu@sqweb.com>
2017-11-03 16:43:10 +0100

    Added button customization in laravel sdk

--- config/sqweb.php
+++ config/sqweb.php
@@ -2,7 +2,7 @@
 
 return [
     'id_site'       => env('SQW_ID_SITE', 00000),
-    'sitename'		=> env('SQW_SITENAME', 'change_this'),
+    'sitename'      => env('SQW_SITENAME', 'change_this'),
     'message'       => env('SQW_MESSAGE', ''),
     'adblock_modal' => env('SQW_ADBLOCK_MODAL', 0),
     'targeting'     => env('SQW_TARGETING', 0),
@@ -10,4 +10,13 @@ return [
     'debug'         => env('SQW_DEBUG', 0),
     'dwide'         => env('SQW_DWIDE', 1),
     'lang'          => env('SQW_LANG', 'en'),
+    'login'         => env('SQW_LOGIN', ''),
+    'connected'     => env('SQW_CONNECTED', ''),
+    'support'       => env('SQW_SUPPORT', ''),
+    'btn_noads'     => env('SQW_BTN_NOADS', ''),
+    'login_tiny'    => env('SQW_LOGIN_TINY', ''),
+    'connected_s'   => env('SQW_CONNECTED_S', ''),
+    'btn_unlimited' => env('SQW_BTN_UNLIMITED', ''),
+    'connected_tiny'=> env('SQW_CONNECTED_TINY', ''),
+    'connected_support'=> env('SQW_CONNECTED_SUPPORT', ''),
 ];
